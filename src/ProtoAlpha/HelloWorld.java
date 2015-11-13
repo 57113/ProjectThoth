13e45b637129d8bca1b096cd505c5b4306dc990c
SHAYB57113 <sbarkham.57113@farnborough.ac.uk>
2015-11-13 13:57:03 +0000

    Import 13/11/2015

---
+++ src/ProtoAlpha/HelloWorld.java
@@ -0,0 +1,13 @@
+package ProtoAlpha;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	helloWorld();
+		
+
+	}
+public static void helloWorld(){
+	System.out.print("Hello World");
+}
+}
