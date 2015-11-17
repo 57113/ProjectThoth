e2d8ce3341c7236c7292c4a3bbb4ec9a9034872f
SHAYB57113 <sbarkham.57113@farnborough.ac.uk>
2015-11-17 10:24:31 +0000

    17/11/2015 10:24
    Log in completed

--- src/ProtoAlpha/commandLine.java
+++ src/ProtoAlpha/commandLine.java
@@ -7,12 +7,27 @@ public class commandLine {
 
 	}
 public static void logIn(){
+	String shaysUsername ="Shay";
+	int shaysPassword = 56734;
 	System.out.println("Please Enter Your Username");
+	@SuppressWarnings("resource")
 	Scanner sc=new Scanner(System.in);
 	String username = sc.next();
-	System.out.println("Please Enter Your Password");
-	String password = sc.next();
-	System.out.println(username);
-	System.out.println(password);
-}
-}
+	int checkOne = 0;
+	if(username.equals(shaysUsername)){
+		System.out.println("Username Accepted");
+		 checkOne = + 1;
+		System.out.println("Please Enter Your Password");
+	int password = sc.nextInt();
+	if(password == shaysPassword){
+		System.out.println("Password Accepted");
+	    checkOne = checkOne +1;}
+		else{
+		System.out.println("Invalid Password Console Locked");}}
+		else{
+		System.out.println("Invalid Username Console Locked");}
+	}
+	}
+
+
+
